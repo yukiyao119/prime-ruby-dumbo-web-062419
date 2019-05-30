26723861886c346328b87f70615d715bbdf97af1
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-05-30 18:41:06 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,11 @@
 # Add  code here!
 def prime?(n)
   mid = floor(Math.sqrt(n)
-  (1..mid).to_a).each do |x|
+  (1..mid).to_a).each { |x|
     if n % x == 0 
       return false
     else 
       return ture 
     end
-  end
+  }
 end
