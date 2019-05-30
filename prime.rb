138daf9d1c85c39ca09092883e28f9c88d916675
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-05-30 18:43:32 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(n)
+  mid = floor(Math.sqrt(n)
+  arr = array (1..mid)
+  arr.each { |x|
+    if n % x == 0 
+      return false
+    else 
+      return ture 
+    end
+  }
+end
